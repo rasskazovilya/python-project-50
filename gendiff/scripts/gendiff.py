a21e7b9af20b1224ee4c1a17a8d6f91316b08b8e
Ilya <rasskazov.ilya.98@gmail.com>
2023-09-12 10:55:11 +0300

    formatting

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -7,10 +7,11 @@ def main():
         prog='gendiff',
         description='Compares two configuration files and shows a difference.',
     )
+
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-
     parser.add_argument('-f', '--format', help='set format of output')
+
     args = parser.parse_args()
     print('Hello World!')
 
