a900da8be916fca5605c933790a371981b2e6dd8
Ilya <rasskazov.ilya.98@gmail.com>
2023-09-12 11:37:18 +0300

    add function "generate_diff"

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -4,6 +4,21 @@ import json
 from pathlib import Path
 
 
+def generate_diff(path1, path2):
+    first_file = json.load(open(path1))
+    second_file = json.load(open(path2))
+
+    deleted_keys = set(first_file.keys()) - set(second_file.keys())
+    added_keys = set(second_file.keys()) - set(first_file.keys())
+    common_keys = set(first_file.keys()).intersection(set(second_file.keys()))
+
+    print(first_file)
+    print(second_file)
+    print(f'Del keys: {deleted_keys}')
+    print(f'Add keys: {added_keys}')
+    print(f'Common keys: {common_keys}')
+
+
 def main():
     parser = argparse.ArgumentParser(
         prog='gendiff',
@@ -16,13 +31,10 @@ def main():
 
     args = parser.parse_args()
 
-    first_file_path = Path.cwd() / 'gendiff' /'scripts' / args.first_file
-    second_file_path = Path.cwd() / 'gendiff' /'scripts' / args.second_file
-    first_file = json.load(open(first_file_path))
-    second_file = json.load(open(second_file_path))
+    first_file_path = Path.cwd() / 'gendiff' / 'scripts' / args.first_file
+    second_file_path = Path.cwd() / 'gendiff' / 'scripts' / args.second_file
 
-    print(first_file)
-    print(second_file)
+    generate_diff(first_file_path, second_file_path)
 
 
 if __name__ == '__main__':
