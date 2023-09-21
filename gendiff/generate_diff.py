a1044878bc54e5cc4799ff67638a23afa2601087
Ilya <rasskazov.ilya.98@gmail.com>
2023-09-21 08:32:07 +0300

    rename variables for files

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -2,30 +2,30 @@ from gendiff.utils import parsing
 
 
 def generate_diff(path1, path2):
-    first_file = parsing.get_file_contents(path1)
-    second_file = parsing.get_file_contents(path2)
+    file1 = parsing.get_file_contents(path1)
+    file2 = parsing.get_file_contents(path2)
 
-    deleted_keys = set(first_file.keys()) - set(second_file.keys())
-    added_keys = set(second_file.keys()) - set(first_file.keys())
-    common_keys = set(first_file.keys()).intersection(set(second_file.keys()))
+    deleted_keys = set(file1.keys()) - set(file2.keys())
+    added_keys = set(file2.keys()) - set(file1.keys())
+    common_keys = set(file1.keys()).intersection(set(file2.keys()))
     changed_keys = {
-        key for key in common_keys if first_file[key] != second_file[key]
+        key for key in common_keys if file1[key] != file2[key]
         }
     unchanged_keys = {
-        key for key in common_keys if first_file[key] == second_file[key]
+        key for key in common_keys if file1[key] == file2[key]
         }
 
     diff = '{\n'
-    for key in sorted(set(first_file.keys()) | set(second_file.keys())):
+    for key in sorted(set(file1.keys()) | set(file2.keys())):
         if key in deleted_keys:
-            diff += f'  - {key}: {first_file[key]}\n'
+            diff += f'  - {key}: {file1[key]}\n'
         if key in added_keys:
-            diff += f'  + {key}: {second_file[key]}\n'
+            diff += f'  + {key}: {file2[key]}\n'
         if key in unchanged_keys:
-            diff += f'    {key}: {first_file[key]}\n'
+            diff += f'    {key}: {file1[key]}\n'
         if key in changed_keys:
-            diff += f'  - {key}: {first_file[key]}\n'
-            diff += f'  + {key}: {second_file[key]}\n'
+            diff += f'  - {key}: {file1[key]}\n'
+            diff += f'  + {key}: {file2[key]}\n'
     diff += '}'
 
     return diff
