7fd22ad6344413ecca79d3ce4c1131e36633f361
Ilya <rasskazov.ilya.98@gmail.com>
2023-09-21 14:30:11 +0300

    add stylish format to print diff

--- gendiff/formats/stylish.py
+++ gendiff/formats/stylish.py
@@ -1,3 +1,28 @@
 def format_diff_stylish(diff):
+    text = format_diff(diff)
+    return '{\n' + text + '}'
+
+
+def format_diff(diff, tab=1):
+    text = ''
     for entry in diff:
-        pass
+        key = entry['key']
+        if entry['state'] == 'added':
+            text += format_line(key, entry["new_value"], '+', tab)
+        elif entry['state'] == 'deleted':
+            text += format_line(key, entry["old_value"], '-', tab)
+        elif entry['state'] == 'changed':
+            text += format_line(key, entry["old_value"], '-', tab)
+            text += format_line(key, entry["new_value"], '+', tab)
+        elif entry['state'] == 'unchanged':
+            text += format_line(key, entry["new_value"], ' ', tab)
+        elif entry['state'] == 'nested':
+            text += ' ' * 4 * tab + '{\n'
+            text += format_diff(entry['children'], tab+1)
+            text += ' ' * 4 * tab + '}\n'
+    return text
+
+
+def format_line(key, value, op, tab):
+    indent = ' ' * (4 * tab - 2)
+    return f'{indent}{op} {key}: {value}\n'
