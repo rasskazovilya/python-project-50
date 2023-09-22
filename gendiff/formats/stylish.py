02a76ac6e2ce9bc1a1b80159a9e9720ec9cc457a
Ilya <rasskazov.ilya.98@gmail.com>
2023-09-22 10:09:41 +0300

    rewrite to format nested entries (still messy)

--- gendiff/formats/stylish.py
+++ gendiff/formats/stylish.py
@@ -7,22 +7,49 @@ def format_diff(diff, tab=1):
     text = ''
     for entry in diff:
         key = entry['key']
+        old_value = entry.get('old_value')
+        new_value = entry.get('new_value')
+        children = entry.get('children')
+
         if entry['state'] == 'added':
-            text += format_line(key, entry["new_value"], '+', tab)
+            text += format_line(key, new_value, '+', tab)
         elif entry['state'] == 'deleted':
-            text += format_line(key, entry["old_value"], '-', tab)
+            text += format_line(key, old_value, '-', tab)
         elif entry['state'] == 'changed':
-            text += format_line(key, entry["old_value"], '-', tab)
-            text += format_line(key, entry["new_value"], '+', tab)
+            text += format_line(key, old_value, '-', tab)
+            text += format_line(key, new_value, '+', tab)
         elif entry['state'] == 'unchanged':
-            text += format_line(key, entry["new_value"], ' ', tab)
+            text += format_line(key, new_value, ' ', tab)
         elif entry['state'] == 'nested':
-            text += ' ' * 4 * tab + '{\n'
-            text += format_diff(entry['children'], tab+1)
-            text += ' ' * 4 * tab + '}\n'
+            text += format_line(key, children, ' ', tab)
     return text
 
 
 def format_line(key, value, op, tab):
     indent = ' ' * (4 * tab - 2)
-    return f'{indent}{op} {key}: {value}\n'
+    text = format_value(value)
+    if isinstance(value, dict):
+        # print(key, value)
+        text = f'{indent}{op} {key}: {"{"}\n'
+        text += ''.join(format_line(k, v, ' ', tab + 1) for k, v in value.items())
+        # for k, v in value.items():
+        #     text += format_line(k, v, ' ', tab + 1)
+        return text + ' ' * 4 * tab + '}\n'
+    elif isinstance(value, list):
+        text = f'{indent}{op} {key}: {"{"}\n'
+        text += format_diff(value, tab + 1)
+        return text + ' ' * 4 * tab + '}\n'
+    return f'{indent}{op} {key}: {format_value(value)}\n'
+
+
+def format_value(value):
+    if value is True:
+        return 'true'
+    elif value is False:
+        return 'false'
+    elif value is None:
+        return 'null'
+    # elif isinstance(value, dict):
+    #     return ''.join(format_line(k, v, ' ', tab + 1) for k, v in value.items())
+    else:
+        return value
