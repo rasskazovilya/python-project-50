73ee5fa08d503f5e6358ec1b29accac3f08b0f8e
Ilya <rasskazov.ilya.98@gmail.com>
2023-09-22 10:40:03 +0300

    add tests for diff plain and json formats

--- gendiff/tests/test_generate_diff.py
+++ gendiff/tests/test_generate_diff.py
@@ -7,7 +7,7 @@ def get_fixture_path(filename):
     return fixtures_folder / filename
 
 
-def test_generate_diff_json():
+def test_gendiff_json():
     diff = generate_diff(
         get_fixture_path('file1.json'), get_fixture_path('file2.json')
     )
@@ -16,7 +16,7 @@ def test_generate_diff_json():
     assert result == diff
 
 
-def test_generate_diff_yml():
+def test_gendiff_yml():
     diff = generate_diff(
         get_fixture_path('file1.yml'), get_fixture_path('file2.yml')
     )
@@ -25,7 +25,7 @@ def test_generate_diff_yml():
     assert result == diff
 
 
-def test_generate_diff_nested_json():
+def test_gendiff_nested_json_stylish():
     diff = generate_diff(
         get_fixture_path('nested_file1.json'),
         get_fixture_path('nested_file2.json')
@@ -35,12 +35,61 @@ def test_generate_diff_nested_json():
         result = result_file.read()
     assert result == diff
 
-def test_generate_diff_nested_yml():
+
+def test_gendiff_nested_yml_stylish():
     diff = generate_diff(
         get_fixture_path('nested_file1.yml'),
         get_fixture_path('nested_file2.yaml')
     )
 
     with open(get_fixture_path('nested_result_stylish.txt')) as result_file:
+        result = result_file.read()
+    assert result == diff
+
+
+def test_gendiff_nested_json_plain():
+    diff = generate_diff(
+        get_fixture_path('nested_file1.json'),
+        get_fixture_path('nested_file2.json'),
+        format='plain'
+    )
+
+    with open(get_fixture_path('nested_result_plain.txt')) as result_file:
+        result = result_file.read()
+    assert result == diff
+
+
+def test_gendiff_nested_yml_plain():
+    diff = generate_diff(
+        get_fixture_path('nested_file1.yml'),
+        get_fixture_path('nested_file2.yaml'),
+        format='plain'
+    )
+
+    with open(get_fixture_path('nested_result_plain.txt')) as result_file:
+        result = result_file.read()
+    assert result == diff
+
+
+def test_gendiff_nested_json_json():
+    diff = generate_diff(
+        get_fixture_path('nested_file1.json'),
+        get_fixture_path('nested_file2.json'),
+        format='json'
+    )
+
+    with open(get_fixture_path('nested_result_json.txt')) as result_file:
+        result = result_file.read()
+    assert result == diff
+
+
+def test_gendiff_nested_yml_json():
+    diff = generate_diff(
+        get_fixture_path('nested_file1.yml'),
+        get_fixture_path('nested_file2.yaml'),
+        format='json'
+    )
+
+    with open(get_fixture_path('nested_result_json.txt')) as result_file:
         result = result_file.read()
     assert result == diff
