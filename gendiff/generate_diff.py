270fe9e459608fdee93e28926202a5ba407af886
Ilya <rasskazov.ilya.98@gmail.com>
2023-09-27 12:01:50 +0300

    change generate_diff module with new utils

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,11 +1,13 @@
-from gendiff.utils import parsing
 from gendiff.formats.choose_format import format_diff
+from gendiff.utils import parsing, read_file
 
 
 def generate_diff(path1, path2, format='stylish'):
-    file1 = parsing.get_file_contents(path1)
-    file2 = parsing.get_file_contents(path2)
-    diff = get_diff(file1, file2)
+    data1, data_format1 = read_file.get_contents_suffix(path1)
+    data2, data_format2 = read_file.get_contents_suffix(path2)
+    parsed_data1 = parsing.parse(data1, data_format1)
+    parsed_data2 = parsing.parse(data2, data_format2)
+    diff = get_diff(parsed_data1, parsed_data2)
     return format_diff(diff, format)
 
 
@@ -29,10 +31,10 @@ def make_new_diff_entry(key, new_value, old_value):
     return entry
 
 
-def get_diff(file1, file2):
+def get_diff(serial_data1, serial_data2):
     diff = []
-    for key in sorted(set(file1.keys()) | set(file2.keys())):
-        value1 = file1.get(key, '_empty')
-        value2 = file2.get(key, '_empty')
+    for key in sorted(set(serial_data1.keys()) | set(serial_data2.keys())):
+        value1 = serial_data1.get(key, '_empty')
+        value2 = serial_data2.get(key, '_empty')
         diff.append(make_new_diff_entry(key, value2, value1))
     return diff
